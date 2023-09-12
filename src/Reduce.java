8bc054eda854340256c86a8907603d4fb2d0af60
Yoshiku Yuu <119818606+YoshikuYuu@users.noreply.github.com>
2023-09-11 20:20:50 -0400

    Added Reduce.java

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        int steps = 0;
+        while (n != 0) {
+            n = ReduceStep(n);
+            steps++;
+        }
+        System.out.println(steps);
+    }
+
+    public static int ReduceStep(int n) {
+        if (n % 2 == 0) {
+            return n / 2;
+        }
+        else {
+            return n - 1;
+        }
+    }
 }
